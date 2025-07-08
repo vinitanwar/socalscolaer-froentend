3e8764638a713b6cb117bfc042fa411baed36bb7
vivek <157991083+jonty1231@users.noreply.github.com>
2025-07-08 10:54:36 +0530

    done

--- src/app/components/reduxstore/utils.js
+++ src/app/components/reduxstore/utils.js
@@ -1,2 +1,4 @@
-export const baseurl="http://localhost:8000/api/v1"
-export const imageurl="http://localhost:8000/storage"
+// export const baseurl="http://localhost:8000/api/v1"
+// export const imageurl="http://localhost:8000/storage"
+export const baseurl="https://admin.social-scholars.com/api/v1"
+export const imageurl="https://admin.social-scholars.com/storage"
