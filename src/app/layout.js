822b9cc5bcf3662e2127a449ccf03b6dfa68d83c
Vinod Tanwar <125426162+vinitanwar@users.noreply.github.com>
2025-09-24 23:03:13 +0530

    google add

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-     
+     <head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2655358665967568"
+     crossorigin="anonymous"></script>
+    </head>
+    
       <body className="antialiased no-copy">
         <CustomeLayout>{children}</CustomeLayout>
       </body>
