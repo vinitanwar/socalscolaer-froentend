e5a2b28cdb4479f0186834b42d71e1a2e0cb11de
Vinod Tanwar <125426162+vinitanwar@users.noreply.github.com>
2025-09-24 23:59:27 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import EditorPicksCarousel from "./components/Editors";
 import MoreNewsSection from "./components/Morenews";
 import InternationalNewsSection from "./components/InternationalNewsSection";
 import Nav from "./components/Nav";
-import Script from "next/script";
+
 
 export default function Home() {
   return (
@@ -19,53 +19,12 @@ export default function Home() {
 
       <Hero />
 
-      {/* Google AdSense Script */}
-      // <Script
-      //   async
-      //   strategy="afterInteractive"
-      //   src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2655358665967568"
-      //   crossOrigin="anonymous"
-      // />
-
-      {/* Ad Banner */}
-      // <div
-      //   className="my-4"
-      //   dangerouslySetInnerHTML={{
-      //     __html: `
-      //       <ins class="adsbygoogle"
-      //            style="display:block"
-      //            data-ad-client="ca-pub-2655358665967568"
-      //            data-ad-slot="6977336533"
-      //            data-ad-format="auto"
-      //            data-full-width-responsive="true"></ins>
-      //       <script>
-      //            (adsbygoogle = window.adsbygoogle || []).push({});
-      //       </script>
-      //     `,
-      //   }}
-      // />
+     
 
       <TopStoriesSection />
       <EditorPicksCarousel />
 
-      {/* Another Ad Banner */}
-      // <div
-      //   className="my-4"
-      //   dangerouslySetInnerHTML={{
-      //     __html: `
-      //       <ins class="adsbygoogle"
-      //            style="display:block"
-      //            data-ad-client="ca-pub-2655358665967568"
-      //            data-ad-slot="6977336533"
-      //            data-ad-format="auto"
-      //            data-full-width-responsive="true"></ins>
-      //       <script>
-      //            (adsbygoogle = window.adsbygoogle || []).push({});
-      //       </script>
-      //     `,
-      //   }}
-      />
-
+    
       <MoreNewsSection />
       <InternationalNewsSection />
     </>
